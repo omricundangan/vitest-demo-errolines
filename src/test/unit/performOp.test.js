b60757bbdb47c147bb2248440fa475faf522655c
Omri Cundangan <omri.cundangan@avoxi.com>
2025-02-25 11:10:20 -0500

    trigger error

--- src/test/unit/performOp.test.js
+++ src/test/unit/performOp.test.js
@@ -1,9 +1,9 @@
 import { expect, it, vi } from "vitest"
 import { performOp } from "./performOp"
 
-// vi.mock('./performOp', () => ({
-//   performOp: vi.fn().mockReturnValue(10),
-// }))
+vi.mock('./performOp', () => ({
+  performOp: vi.fn().mockReturnValue(10),
+}))
 
 it('tests performOp module', () => {
   const adder = performOp
